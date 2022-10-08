f79a1fc82efb9d93b56e59584b957877b4940e93
fuyang0115 <2684072459@qq.com>
2022-10-08 21:49:31 +0800

    允许跨域

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
@@ -12,6 +13,21 @@ import java.util.List;
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//设置允许跨域的路径
+                //如果有多个路径需要跨域，只需要将跨域路径放入数组中
+                //String []  allowDomain={"http://**","http://*","http://*"};
+                //.allowedOrigins(allowDomain)//多url跨域
+                .allowedOriginPatterns("*")//设置允许跨域请求的域名
+                .allowCredentials(true)//是否允许证书 不写默认开启
+                .allowedMethods("*") //设置允许的方法
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                //暴露哪些原始请求头部信息
+                .exposedHeaders("*")
+                .maxAge(3600);//跨域允许时间
+    }
 
     /**
      * 扩展mvc框架的消息转换器
