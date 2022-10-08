832d33ce867e504757b89c30dfbae82632902dbb
Niyiming <1051119007@qq.com>
2022-10-08 23:48:06 +0800

    调试接口

--- pages/index/association/my_activity/index.vue
+++ pages/index/association/my_activity/index.vue
@@ -6,29 +6,28 @@
                     </view>
                   </view> -->
             <!-- 社团活动卡片 -->
-			
-            <view class="cu-card dynamic" v-if="act_list" @tap="gotoSchedule" :data-content="item" v-for="(item, index) in (act_list)" :key="index">
+            <view class="cu-card dynamic" v-if="act_list"  :data-content="item" v-for="(item,index) in (act_list)" :key="index" @click="gotoSchedule(item.index)">
                 <view class="cu-item shadow" style="margin: 30rpx">
                     <view class="cu-list menu-avatar">
                         <view class="cu-item">
-                            <image src="https://img-blog.csdnimg.cn/20210512171051865.png" class="cu-avatar round lg ttt"></image>
-							 <!-- ↑改：请求社团头像<image src="item.content.logoUrl" class="cu-avatar round lg ttt"></image> -->
+                            <image :src="item.content.logoUrl" class="cu-avatar round lg ttt"></image>
+							 
                             <!-- <view class="cu-avatar round lg ttt" style="background-image:url({{item.url}});"></view> -->
                             <view class="content flex-sub">
                                 <view>{{ item.content.association[0].detail }}</view>
                                 <view class="text-gray text-sm flex justify-between">
-                                    {{ item.content.time }}
+                                    {{ item.time }}
                                 </view>
                             </view>
-                            <view class="ttt" :style="'margin-right: 25rpx; font-size: 28rpx; font-weight: 550;  color: ' + (item.content.status == '通过' ? '#98c898' : '#cc463d') + ';'">{{ item.content.status }}</view>
+                            <view class="ttt" :style="'margin-right: 25rpx; font-size: 28rpx; font-weight: 550;  color: ' + (item.status == '通过' ? '#98c898' : '#cc463d') + ';'">{{ item.status }}</view>
                         </view>
                     </view>
                     <view class="text-content">
-                        {{ item.content.Title }}
+                        {{ item.Title }}
                     </view>
                     <view class="grid flex-sub padding-lr col-1">
-						<image src="https://img-blog.csdnimg.cn/20210512171051865.png" class="bg-img only-img ttt" mode="aspectFill"></image>
-                        <!-- ↑改：请求cover图片 <image src="item.content.Cover" class="bg-img only-img ttt" mode="aspectFill"></image> -->
+						<image :src="item.content.Cover" class="bg-img only-img ttt" mode="aspectFill"></image>
+                        
                         <!-- <view class="bg-img only-img ttt" style="background-image:url({{item.image}});" wx:key="{{index}}">
                         </view> -->
                     </view>
@@ -42,35 +41,42 @@
 </template>
 
 <script>
-	// pages/association_new/activity_schedule/activity_schedule.js
+
 		export default {
-			data() {
-				return {	
-					act_list:[
-						{content:{
-							Title:"纳新活动",
-                            time:"2022.10.10",
-							association: [
-							 {name:"swimcl",detail:"游泳社"},],
-							 
-							 Cover:"",
-							 logoUrl: "Image(images/hyjj.jpg)",
-							 status:"未通过",
-							   }},
-						{content:{
-							   	Title:"纳新活动",
-							       time:"2022.10.5",
-							   	association: [
-							   	 {name:"runcl",detail:"跑步社"},],
-							   	 
-							   	 Cover:"cover喽",
-							   	 
-							   	 status:"通过",
-							   	   }},
-							]
-							
-				}
+			  data() {
+				  
+			  	return {
+			  	act_list:[]
+				
+			  	};
 			  },
+
+			  methods: {
+			  	gotoSchedule(e) {
+			       console.log(e);
+			  		uni.navigateTo({
+			  			url:"/pages/schedule/schedule?index="+e
+			  					})
+			  	        },
+				
+				getData(){
+					uni.request({
+						
+						url:"http://127.0.0.1:4523/m1/1710071-0-default/my_activity/list",
+						method:"POST",
+						success:res=>{
+						    console.log(res)
+								  		this.act_list=res.data.result.data.data
+								   },
+				})
+				}
+				
+			  },
+			  
+			  onLoad() {
+			     this.getData();
+			  },
+			  
 			  /**
 			  	* 生命周期函数--监听页面初次渲染完成
 			  	*/
@@ -99,11 +105,12 @@
 			  	* 用户点击右上角分享
 			  	*/
 			  onShareAppMessage() {},
-			  methods: {}
 				}
 		
 </script>
 
 <style lang="scss">
 	@import "./index.css";
+
 </style>
+
